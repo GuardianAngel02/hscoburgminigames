d2c955c8d226279e2e8f2a3f4dbacd826e4ee800
GuardianAngel02 <57775342+GuardianAngel02@users.noreply.github.com>
2019-11-15 17:05:20 +0100

    corrected preload paths
    /hscoburgminigames

--- Minigames/Platformer/platformerFinalSceneless.js
+++ Minigames/Platformer/platformerFinalSceneless.js
@@ -44,21 +44,21 @@ function StartPlatformerSceneless(){
   function preload ()
   {
       //this.load.image('sky', '../../data/Platformer/one/Level_1_Sky2.png');
-      this.load.image('sky', '/Minigames/Platformer/images/Level_1_Sky2.png');
-      this.load.image('platform200', '/Minigames/Platformer/images/Level_1_Platform_200.png');
-      this.load.image('platform400', '/Minigames/Platformer/images/Level_1_Platform_400.png');
-      this.load.image('platform600', '/Minigames/Platformer/images/Level_1_Platform_600.png');
-      this.load.image('floor', '/Minigames/Platformer/images/Level_1_Floor2.png');
-      this.load.image('star', '/Minigames/Platformer/images/star.png');
-      this.load.image('bomb', '/Minigames/Platformer/images/bomb.png');
-      this.load.spritesheet('dude', '/Minigames/Platformer/images/dude2.png', { frameWidth: 32, frameHeight: 42 });
+      this.load.image('sky', '/hscoburgminigames/Minigames/Platformer/images/Level_1_Sky2.png');
+      this.load.image('platform200', '/hscoburgminigames/Minigames/Platformer/images/Level_1_Platform_200.png');
+      this.load.image('platform400', '/hscoburgminigames/Minigames/Platformer/images/Level_1_Platform_400.png');
+      this.load.image('platform600', '/hscoburgminigames/Minigames/Platformer/images/Level_1_Platform_600.png');
+      this.load.image('floor', '/hscoburgminigames/Minigames/Platformer/images/Level_1_Floor2.png');
+      this.load.image('star', '/hscoburgminigames/Minigames/Platformer/images/star.png');
+      this.load.image('bomb', '/hscoburgminigames/Minigames/Platformer/images/bomb.png');
+      this.load.spritesheet('dude', '/hscoburgminigames/Minigames/Platformer/images/dude2.png', { frameWidth: 32, frameHeight: 42 });
 
-      this.load.audio('jump', '/Minigames/Platformer/sounds/appear-online.ogg');
-      this.load.audio('boom', '/Minigames/Platformer/sounds/bomb_explosion.ogg');
-      this.load.audio('levelup', '/Minigames/Platformer/sounds/levelup.ogg');
-      this.load.audio('coin', '/Minigames/Platformer/sounds/Coin.ogg');
+      this.load.audio('jump', '/hscoburgminigames/Minigames/Platformer/sounds/appear-online.ogg');
+      this.load.audio('boom', '/hscoburgminigames/Minigames/Platformer/sounds/bomb_explosion.ogg');
+      this.load.audio('levelup', '/hscoburgminigames/Minigames/Platformer/sounds/levelup.ogg');
+      this.load.audio('coin', '//hscoburgminigamesMinigames/Platformer/sounds/Coin.ogg');
 
-      this.load.audio('music', '/Minigames/Platformer/sounds/Circus.mp3');
+      this.load.audio('music', '/hscoburgminigames/Minigames/Platformer/sounds/Circus.mp3');
   }
 
   function create ()
