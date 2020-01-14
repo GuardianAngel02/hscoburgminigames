b81d92c4bc1ee6d2650ad7fdc4ff7fa431574244
GuardianAngel02 <57775342+GuardianAngel02@users.noreply.github.com>
2020-01-14 23:13:29 +0100

    updated links to assets

--- Minigames/Untitled_Space_Game/Loading.js
+++ Minigames/Untitled_Space_Game/Loading.js
@@ -8,36 +8,36 @@ class Loading extends Phaser.Scene {
     this.add.text(20, 20, "Loading Game...");
     console.log("loading assets");
 
-    this.load.image('space', 'Minigames/Untitled_Space_Game/images/outerspace.png');
-    this.load.image('sky', 'Minigames/Untitled_Space_Game/images/sky.png');
-    this.load.image('grass', 'Minigames/Untitled_Space_Game/images/Grass.png');
-    this.load.image('sea', 'Minigames/Untitled_Space_Game/images/sea.png');
+    this.load.image('space', '/hscoburgminigames/Minigames/Untitled_Space_Game/images/outerspace.png');
+    this.load.image('sky', '/hscoburgminigames/Minigames/Untitled_Space_Game/images/sky.png');
+    this.load.image('grass', '/hscoburgminigames/Minigames/Untitled_Space_Game/images/Grass.png');
+    this.load.image('sea', '/hscoburgminigames/Minigames/Untitled_Space_Game/images/sea.png');
 
-    this.load.image('clamps', 'Minigames/Untitled_Space_Game/images/clamps.png');
-    this.load.image('clamps_broken', 'Minigames/Untitled_Space_Game/images/clamps_broken.png');
+    this.load.image('clamps', '/hscoburgminigames/Minigames/Untitled_Space_Game/images/clamps.png');
+    this.load.image('clamps_broken', '/hscoburgminigames/Minigames/Untitled_Space_Game/images/clamps_broken.png');
 
 
-    this.load.spritesheet('ship', 'Minigames/Untitled_Space_Game/images/spaceshipsprite.png',{
+    this.load.spritesheet('ship', '/hscoburgminigames/Minigames/Untitled_Space_Game/images/spaceshipsprite.png',{
       frameWidth: 58,
       frameHeight: 120
     });
 
-    this.load.spritesheet('radar', 'Minigames/Untitled_Space_Game/images/radarsprite.png',{
+    this.load.spritesheet('radar', '/hscoburgminigames/Minigames/Untitled_Space_Game/images/radarsprite.png',{
       frameWidth: 38,
       frameHeight: 30
     });
 
 
-    this.load.image('ship_idle', 'Minigames/Untitled_Space_Game/images/spaceshipidle.png');
-    this.load.image('plane', 'Minigames/Untitled_Space_Game/images/plane.png');
+    this.load.image('ship_idle', '/hscoburgminigames/Minigames/Untitled_Space_Game/images/spaceshipidle.png');
+    this.load.image('plane', '/hscoburgminigames/Minigames/Untitled_Space_Game/images/plane.png');
 
 
-    this.load.audio('click', ['/Minigames/Untitled_Space_Game/sounds/click.ogg', '/Minigames/Untitled_Space_Game/sounds/click.mp3']);
-    this.load.audio('boom', ['/Minigames/Untitled_Space_Game/sounds/bomb_explosion.ogg', '/Minigames/Untitled_Space_Game/sounds/bomb_explosion.mp3']);
+    this.load.audio('click', ['/hscoburgminigames/Minigames/Untitled_Space_Game/sounds/click.ogg', '/hscoburgminigames/Minigames/Untitled_Space_Game/sounds/click.mp3']);
+    this.load.audio('boom', ['/hscoburgminigames/Minigames/Untitled_Space_Game/sounds/bomb_explosion.ogg', '/hscoburgminigames/Minigames/Untitled_Space_Game/sounds/bomb_explosion.mp3']);
 
-    this.load.audio('music', ['/Minigames/Untitled_Space_Game/sounds/8bit-OnTheOffensiveV2.ogg', '/Minigames/Untitled_Space_Game/sounds/8bit-OnTheOffensiveV2.mp3']);
+    this.load.audio('music', ['/hscoburgminigames/Minigames/Untitled_Space_Game/sounds/8bit-OnTheOffensiveV2.ogg', '/hscoburgminigames/Minigames/Untitled_Space_Game/sounds/8bit-OnTheOffensiveV2.mp3']);
 
-    this.load.audio('ambient', ['/Minigames/Untitled_Space_Game/sounds/ambient.ogg', '/Minigames/Untitled_Space_Game/sounds/ambient.mp3']);
+    this.load.audio('ambient', ['/hscoburgminigames/Minigames/Untitled_Space_Game/sounds/ambient.ogg', '/hscoburgminigames/Minigames/Untitled_Space_Game/sounds/ambient.mp3']);
 
 
   }
